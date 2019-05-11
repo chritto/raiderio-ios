c022518d28c3edf8870384a0d1ead9ef1446c29b
Christopher McGrath <chritto@ieee.org>
2019-05-11 14:48:23 -0700

    Get the app compiling again, add manual IGListDiffable conformance, regenerate value objects

---
+++ RaiderIO/Screens/CharacterList/RIOCharacterPreviewViewModel+IGListDiffable.h
@@ -0,0 +1,17 @@
+//
+//  RIOCharacterPreviewViewModel+IGListDiffable.h
+//  RaiderIO
+//
+//  Created by Chris McGrath on 5/11/19.
+//  Copyright © 2019 Chris McGrath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RIOCharacterPreviewViewModel_IGListDiffable : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RaiderIO/Screens/CharacterList/RIOCharacterPreviewViewModel+IGListDiffable.m
@@ -0,0 +1,13 @@
+//
+//  RIOCharacterPreviewViewModel+IGListDiffable.m
+//  RaiderIO
+//
+//  Created by Chris McGrath on 5/11/19.
+//  Copyright © 2019 Chris McGrath. All rights reserved.
+//
+
+#import "RIOCharacterPreviewViewModel+IGListDiffable.h"
+
+@implementation RIOCharacterPreviewViewModel_IGListDiffable
+
+@end
